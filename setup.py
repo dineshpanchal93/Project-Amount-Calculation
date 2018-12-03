981ebbe072b447641267a30a0730d633fa7b17a4
Dinesh Panchal <dineshpanchal432@gmail.com>
2018-12-03 11:45:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ import re, ast
 # get version from __version__ variable in project_amount_calculation/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
 with open('project_amount_calculation/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
@@ -21,6 +24,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=f.read().strip().split('\n'),
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
